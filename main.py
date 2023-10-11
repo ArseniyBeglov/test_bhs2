14997a0ee9b02572412d1b11572fe6f3f4777d15
ArseniyBeglov <HUDAIDOTT@yandex.ru>
2023-10-11 18:16:57 +0300

    add Automatic User Loading

--- main.py
+++ main.py
@@ -2,12 +2,10 @@ from flask import Flask, jsonify, request, flash, session, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_swagger_ui import get_swaggerui_blueprint
 from passlib.hash import sha256_crypt
-from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity, current_user
 
 SWAGGER_URL = '/api/docs'
 API_URL = '/static/swagger.json'
-
-
 
 db = SQLAlchemy()
 app = Flask(__name__)
@@ -68,6 +66,17 @@ with app.app_context():
     db.create_all()
 
 
+@jwt.user_identity_loader
+def user_identity_lookup(user):
+    return user.id
+
+
+@jwt.user_lookup_loader
+def user_lookup_callback(_jwt_header, jwt_data):
+    identity = jwt_data["sub"]
+    return User.query.filter_by(id=identity).one_or_none()
+
+
 @app.route('/login', methods=['POST'])
 def login():
     username = request.form.get('username')
@@ -76,15 +85,15 @@ def login():
     user = User.query.filter_by(username=username).first()
     if user:
         if sha256_crypt.verify(password, user.hased_password):
-            access_token = create_access_token(identity=user.id)
-            return jsonify(message='Login Successful', access_token=access_token),200
+            access_token = create_access_token(identity=user)
+            return jsonify(message='Login Successful', access_token=access_token), 200
         else:
-            return jsonify('first'), 401
+            return jsonify('not correct username or password'), 401
     else:
-        return jsonify('second'), 401
+        return jsonify('not correct username or passwoord'), 401
 
 
-@app.route('/logout')
+@app.route('/logout', methods=['DELETE'])
 def logout():
     return redirect('/')
 
@@ -97,7 +106,7 @@ def sing_up():
         password2 = request.form.get('password2')
         print(username)
         user = User.query.filter_by(username=username).first()
-        print(user)
+
         if user:
             return jsonify('Bad email or Password'), 401
         elif len(username) < 2:
@@ -133,30 +142,54 @@ def update_top_assets_cache(user_id):
     top_assets_cache[user.id] = list
 
 
-@app.route('/asset/by-user')
+@app.route('/user', methods=['GET'])
+@jwt_required()
+def get_user_info():
+    return jsonify(id=current_user.id,
+                   username=current_user.username), 200
+
+
+@app.route('/update/user', methods=['PUT'])
+@jwt_required()
+def update_user():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    if len(username) < 1:
+        return jsonify(message='user not updated successfully'), 401
+    else:
+        user = User.query.get(current_user.id)
+        user.username = username
+        user.hased_password = sha256_crypt.hash(password)
+        db.session.commit()
+        return jsonify(message='user updated successfully'), 201
+
+@app.route('/asset/by-user', methods=['GET'])
 @jwt_required()
 def home():
-    user_assets = Asset.query.filter_by(owner_id=get_jwt_identity()).all()
+    user_assets = Asset.query.filter_by(owner_id=current_user.id).all()
     list = []
     for asset in user_assets:
         list.append([asset.id, asset.name, asset.description, asset.price, asset.owner_id])
     return jsonify(list), 200
 
-@app.route('/asset/by-user/<int:asset_id>')
+
+@app.route('/asset/by-user/<int:asset_id>', methods=['GET'])
 @jwt_required()
 def asset_by_id(asset_id):
     asset = Asset.query.filter_by(id=asset_id).first()
-    list = [asset.id, asset.name, asset.description, asset.price, asset.owner_id]
-    return jsonify(list), 200
+    if asset is not None:
+        list = [asset.id, asset.name, asset.description, asset.price, asset.owner_id]
+        return jsonify(list), 200
 
-@app.route('/asset/top')
+
+@app.route('/asset/top', methods=['GET'])
 @jwt_required()
 def top():
-    user_assets = Asset.query.filter_by(owner_id=get_jwt_identity()).order_by(Asset.price.desc()).limit(5).all()
-    list=[]
+    user_assets = Asset.query.filter_by(owner_id=current_user.id).order_by(Asset.price.desc()).limit(5).all()
+    list = []
     for asset in user_assets:
-        list.append([asset.id,asset.name,asset.description,asset.price,asset.owner_id])
-    return jsonify(list),200
+        list.append([asset.id, asset.name, asset.description, asset.price, asset.owner_id])
+    return jsonify(list), 200
 
 
 @app.route('/asset/add', methods=['POST'])
@@ -166,12 +199,12 @@ def add_asset():
     description = request.form.get('description')  # Поле для описания ассета
     price = request.form.get('price')
 
-    update_top_assets_cache(get_jwt_identity())
+    update_top_assets_cache(current_user.id)
 
     if len(name) < 1:
         return jsonify('Bad name'), 401
     else:
-        new_asset = Asset(name=name, description=description, price=price, owner_id=get_jwt_identity())
+        new_asset = Asset(name=name, description=description, price=price, owner_id=current_user.id)
         db.session.add(new_asset)
         db.session.commit()
         return jsonify(message='asset created successfully'), 201
@@ -181,18 +214,19 @@ def add_asset():
 @jwt_required()
 def delete_asset(asset_id):
     asset = Asset.query.get(asset_id)
-    if asset and asset.owner_id == get_jwt_identity():
+    if asset and asset.owner_id == current_user.id:
         db.session.delete(asset)
         db.session.commit()
-        update_top_assets_cache(get_jwt_identity())
+        update_top_assets_cache(current_user.id)
         return jsonify(message='asset deleted successfully'), 201
     else:
         return jsonify(message='asset not deleted '), 401
 
+
 @app.route('/asset/update/<int:asset_id>', methods=['PUT'])
 @jwt_required()
 def update_asset(asset_id):
-    name = request.form.get('name') # Поле для имени ассета
+    name = request.form.get('name')  # Поле для имени ассета
     description = request.form.get('description')  # Поле для описания ассета
     price = request.form.get('price')
     if len(name) < 1:
@@ -205,19 +239,18 @@ def update_asset(asset_id):
         db.session.commit()
         return jsonify(message='asset updated successfully'), 201
 
+
+
 @app.route('/asset/buy/<int:asset_id>', methods=['PUT'])
 @jwt_required()
 def buy_asset(asset_id):
-    owner_id = request.form.get('owner_id')
-
-    if Asset.query.get(asset_id) is None or User.query.filter_by(id=owner_id) is None:
+    if Asset.query.get(asset_id) is None or User.query.filter_by(id=current_user.id) is None:
         return jsonify(message='asset not bought successfully'), 401
     else:
         asset = Asset.query.get(asset_id)
-        asset.owner_id = owner_id
+        asset.owner_id = current_user.id
         db.session.commit()
         return jsonify(message='asset bought successfully'), 201
-
 
 
 if __name__ == '__main__':
